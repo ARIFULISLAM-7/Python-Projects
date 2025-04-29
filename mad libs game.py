d35d061b39a27779feb9a4d4352a29cba383f374
Ariful Islam <ariful.pacs@.gmail.com>
2025-04-29 23:30:22 +0600

    mad libs game

---
+++ mad libs game.py
@@ -0,0 +1,8 @@
+noun1 = input("Enter a noun: ")
+noun2 = input("Enter another noun: ")
+noun3 = input("Enter one more noun: ")
+adjective1 = input("Enter an adjective: ")
+adjective2 = input("Enter another adjective: ")
+adjective3 = input("Enter one more adjective: ")
+
+print(f"Welcome to the Mad Libs game! Today, I go to the {noun1}. The place was very {adjective1}. I saw a {noun2} and a {noun3}. I was very {adjective2}. They were very {adjective3}.")
