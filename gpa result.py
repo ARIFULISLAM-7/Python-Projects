5a6106ca9d334c3e0eb407fbf17108c4789566b7
Ariful Islam <ariful.pacs@.gmail.com>
2025-05-05 20:03:02 +0600

    New!

--- gpa result.py
+++ gpa result.py
@@ -1,14 +1,14 @@
+Assignments = 20
+Test = 70
+Lab_Works = 10
 result = int(input("Enter your marks: "))
 if result == 100:
-    print("Your grade is A+ , Excellent!")
+    print("Your grade is A+ , Excellent! You taked the Test, provided Assignments, and did Lab Works.")
 elif result >= 90:
-    print("Your grade is A , Very Good!")
+    print("Your grade is A , Very Good! You taked the Test, and provided Assignments.")
 elif result >= 80:
-    print("Your grade is B , Good!")
+    print("Your grade is B , Good! You taked the Test, and did Lab works.")
 elif result >= 70:
-    print("Your grade is C , Average!")
-elif result >= 60:
-    print("Your grade is D , Below Average!")
+    print("Your grade is C , Average! You taked the Test only.")
 else:
-    print("Your grade is F , Fail!")
-
+    print("Your grade is F , Fail! You did not take the Test, Assignments, and Lab Works.")
